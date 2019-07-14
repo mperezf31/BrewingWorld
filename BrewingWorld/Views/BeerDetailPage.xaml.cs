a4098144af683d9c95aade6407b9a4af48403052
mperezf <miguelperezfernandez31@gmail.com>
2019-07-14 19:14:59 +0200

    Crear animaciones

--- BrewingWorld/Views/BeerDetailPage.xaml.cs
+++ BrewingWorld/Views/BeerDetailPage.xaml.cs
@@ -20,8 +20,14 @@ namespace BrewingWorld.Views
         {
             base.OnAppearing();
 
-            Animation animation = new Animation(a => BeerImage.RotationX = a, 0, 360);
-            animation.Commit(this, "ImageAnimation", 5, 1000, Easing.SinInOut, (arg1, arg2) => { BeerImage.RotationX = 0; }, () => false);
+            Animation animation = new Animation(a => BeerImage.ScaleX = a, 0, 1);
+            animation.Commit(this, "ImageAnimation", 5, 800, Easing.SinInOut, (arg1, arg2) => { BeerImage.ScaleX = 1; }, () => false);
+
+            Animation animationDescription = new Animation(a => Description.Scale = a, 0.8, 1);
+            animationDescription.Commit(this, "DescriptionAnimation", 5, 800, Easing.BounceOut, (arg1, arg2) => { Description.Scale = 1; }, () => false);
+
+            Animation animationStyle = new Animation(a => Style.Scale = a, 0.8, 1);
+            animationStyle.Commit(this, "StyleAnimation", 5, 800, Easing.BounceOut, (arg1, arg2) => { Style.Scale = 1; }, () => false);
 
         }
 
